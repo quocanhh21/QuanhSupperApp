da18af35ed3b615cd10d763ac0c56a5c98380461
anh.le <anh.le@s3corp.com.vn>
2025-03-25 16:42:05 +0700

    test trigger pipeline

--- Infrastructure/Authentication/JwtMiddleware.cs
+++ Infrastructure/Authentication/JwtMiddleware.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
-using System.Net.Http;
 using System.Text;
 
 namespace Infrastructure.Authentication
